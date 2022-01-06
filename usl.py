50c44900ab4e0e76ce456a62e811d076dc9eb52b
Devon Wasson <devonwasson@gmail.com>
2022-01-06 07:51:58 +0000

    usl now sends mod mail messages to the corresponding sub AND the USL sub if a mod of a sub is put on to the USL

--- usl.py
+++ usl.py
@@ -76,12 +76,19 @@ def ban_from_queue(sub_config):
 				users_to_descriptions[user]['mod note'] = "USL ban from r/" + user_data['banned_on'] + " - " + user_data['description'] + " - "
 			users_to_descriptions[user]['mod note'] += "#" + tag + " "
 			users_to_descriptions[user]['description'] = "You have been banned from r/" + sub_config.subreddit_name + " due to a ban from r/" + user_data['banned_on'] + ". You must contact the mods of r/" + user_data['banned_on'] + " to have this ban removed. Please do not reply to this message."
+	mods = [x.name.lower() for x in sub_config.subreddit_object.moderator()]
 	for user in users_to_descriptions:
 		text = users_to_descriptions[user]
 		if user in DO_NOT_BAN:
 			continue
+		if user.lower() in mods:
+			message_content = "Hello, mods of r/" + sub_config.subreddit_name + ". Recently, u/" + user + " was added to the USL with the following context: \n\n> " + text['mod note'] + "\n\nConsidering this user is a **moderator** of your community, it is **imperative** that you figure out what happened.\n\nIf there was a misunderstanding, please reach out to the mods of r/UniversalScammerList to get this sorted out. However, If this user is no longer fit to be a moderator, they should be removed as a moderator and banned from your community."
+			sub_config.subreddit_object.message("One of your moderators has been added to the Universal Scammer List", message_content)
+			message_content = "Hello, USL Mods. Recently, u/" + user + " was added to the USL with the following context: \n\n> " + text['mod note'] + "\n\nHowever, this user is a moderator of r/" + sub_config.subreddit_name + " which is a USL-participating sub.\n\nPlease look into this matter ASAP. If the user in question is not fit to be a moderator and they are **not** removed from the sub(s) in which they moderate, please remove those sub(s) from the USL."
+			sub_config.reddit.subreddit('universalscammerlist').message("Moderator of r/" + sub_config.subreddit_name + " added to the USL", message_content)
+			continue
 		ban_note = "".join([ban.note for ban in sub_config.subreddit_object.banned(redditor=user)]).lower()
-		if not any(["#"+_tag in ban_note for _tag in TAGS]):
+		if ban_note and not any(["#"+_tag in ban_note for _tag in TAGS]):
 			message_content = "Hello, mods of r/" + sub_config.subreddit_name + ". Recently, u/" + user + " was added to the USL with the following context: \n\n> " + text['mod note'] + "\n\nHowever, this user was previously banned on your subreddit through unrelated means. At this time, no action is required. The ban against this user on your sub is not being modified.\n\nHowever, if you wish to modify this ban to be in line with the USL, please modify the ban for this user to include the tags mentioned above. This will sync your ban with the USL so, if this user is taken off the USL in the future, they will be unbanned from your sub as well. If you do NOT wish for this to happen and want this user to remain banned, even if they are removed from the USL, then no action is needed on your part."
 			sub_config.subreddit_object.message("Duplicate Ban Found By USL", message_content)
 			continue
@@ -115,7 +122,10 @@ def get_messages(reddit):
 def publish_unbans(sub_config):
 	messages = get_messages(sub_config.reddit)
 	for message in messages:
-		requester = message.author.name.lower()
+		try:
+			requester = message.author.name.lower()
+		except AttributeError as e:  # Messages without names aren't worth checking
+			continue
 		text = message.body.strip().lower()
 		words = text.split(" ")
 		unbanned_user = ""
@@ -160,7 +170,7 @@ def unban_from_queue(sub_config):
 		users += to_unban[tag]
 	for user in list(set(users)):
 		ban_note = "".join([ban.note for ban in sub_config.subreddit_object.banned(redditor=user)]).lower()
-		if not any(["#"+_tag in ban_note for _tag in TAGS]):
+		if ban_note and not any(["#"+_tag in ban_note for _tag in TAGS]):
 			message_content = "Hello, mods of r/" + sub_config.subreddit_name + ". Recently, u/" + user + " was removed from the USL. However, you banned this user for unrelated reasons. As such, I will not remove this ban for you. However, if you banned this user because you believed them to be a scammer, please double check things as the situation might have changed. Thanks!"
 			sub_config.subreddit_object.message("Conflicting Unban Found In The USL", message_content)
 			continue

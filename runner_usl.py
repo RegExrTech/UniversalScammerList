a333bb3cece20a3b057f8443b2bf607b656d910c
Devon Wasson <devonwasson@gmail.com>
2022-10-29 00:58:20 +0000

    fix runner to avoid collisions with subs that start with the same name

--- runner_usl.py
+++ runner_usl.py
@@ -13,7 +13,7 @@ def main():
                 time.sleep(30)
 
 time.sleep(3)
-ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python3\ runner_usl.py\ ' + subreddit_name).read().splitlines() if 'grep' not in x]
+ps_output = [x for x in os.popen('ps -ef | grep \&\&\ python3\ runner_usl.py\ ' + subreddit_name + "\ ").read().splitlines() if 'grep' not in x]
 # If the only output we get from grep is the grep itself and this instance of the runner,
 # then runner is not currently running so this instance should take over
 if len(ps_output) == 1:

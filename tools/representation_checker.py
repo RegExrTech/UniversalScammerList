9be9a87cc5f9c85a0dd9d2aaf50cdeadaf33d7b4
Devon Wasson <devonwasson@gmail.com>
2023-02-12 16:34:39 +0000

    bug fixes for representation_checker.py

--- tools/representation_checker.py
+++ tools/representation_checker.py
@@ -31,21 +31,22 @@ def main():
 			continue
 
 		if not any([x.name.lower() in usl_sub_mods for x in mod_list]):
+			unrepresented_subs.append(subname)
+
 			title = "[PLEASE READ] r/" + subname + " is not represented in r/UniversalScammerList"
 
 			offender_body = "Hello, mods of r/" + subname + ". Your subreddit is a participating subreddit of the Universal Scammer List. However, none of your mods are moderators in r/UniversalScammerList. The USL requires that at least one **ACTIVE** moderator from your sub also be a moderator of r/UniversalScammerList. This ensures that your sub is properly represented and issues regarding your sub can be dealt with swiftly.\n\n"
 			offender_body += "Please nominate one of your moderators to join r/UniversalScammerList and send your selection as a mod mail message to r/UniversalScammerList. Once you've done so, that user will be invited as a mod shortly. If you do **not** choose a mod to represent your sub, you could lose access to the USL.\n\n"
 			offender_body += "Thank you for your help. Please reach out to the mod team of r/UniversalScammerList if you have any questions!"
 			try:
-				pass
-#				usl_sub_config.reddit.subreddit(sub_config.subreddit_name).message(subject=title, message=offender_body)
+				usl_sub_config.reddit.subreddit(sub_config.subreddit_name).message(subject=title, message=offender_body)
 			except Exception as e:
 				print("Unable to send message to r/" + sub_config.subreddit_name + " with error " + str(e))
-#			time.sleep(30)
+			time.sleep(30)
 
 	title = "The following subs are not represented in r/UniversalScammerList"
 	usl_sub_body = "Hello, mods of r/UniversalScammerList. During a routine inspection, I noticed that none of the mods from the follow subs are  represented on r/UniversalScammerList:\n\n"
-	usl_sub_body += "* " + "\n\n* ".join() + "\n\n"
+	usl_sub_body += "* r/" + "\n\n* r/".join(unrepresented_subs) + "\n\n"
 	usl_sub_body += "I've sent a message to the above subs informing them that they should nominate one of their active mods to represent their sub on r/UniversalScammerList and that failure to do so may result in their sub losing USL access.\n\n"
 	usl_sub_body += "This message is informational and no action is required at this time. However, if the subs fails to nominate moderators to represent their sub, a discussion should be had about their sub having access to the USL.\n\n"
 	usl_sub_body += "Thank you for your help and participation!"

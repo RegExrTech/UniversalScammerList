1d2e5ec1cb84a58c905b6800823f3af40603f5d2
Devon Wasson <devonwasson@gmail.com>
2024-02-29 20:18:20 +0000

    main runner now runs funkoppopmod as it does not have read/write so it's a noop

--- main_runner_usl.py
+++ main_runner_usl.py
@@ -7,11 +7,10 @@ import Config
 subnames = [x.split(".")[0] for x in os.listdir("config/")]
 ps_output = [x for x in os.popen('ps -ef | grep python3\ runner_usl.py\ ').read().splitlines() if 'grep' not in x]
 for subname in subnames:
-	if subname == 'funkoppopmod':
+	if any([x.endswith(" " + subname) for x in ps_output]):
 		continue
-	if not any([x.endswith(" " + subname) for x in ps_output]):
-		config = Config.Config(subname)
-		# Skip running on subs without read and write access
-		if not config.read_from and not config.write_to:
-			continue
-		subprocess.Popen(['python3', 'runner_usl.py', subname])
+	config = Config.Config(subname)
+	# Skip running on subs without read and write access
+	if not config.read_from and not config.write_to:
+		continue
+	subprocess.Popen(['python3', 'runner_usl.py', subname])
